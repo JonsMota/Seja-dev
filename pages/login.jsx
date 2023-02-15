52684f2ba94509d9d062fae1dc7a8ffef16245b1
JonsMota <jonsmota@gmail.com>
2023-02-14 22:43:58 -0300

    1

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import {useForm} from 'react-hook-form'
 import { joiResolver } from '@hookform/resolvers/joi'
@@ -32,8 +33,11 @@ function LoginPage () {
     resolver: joiResolver(loginSchema)
   })
 
+  const [loading, setLoading] = useState(false)
+
   const onSubmit = async (data) => {
     try {
+      setLoading(true)
       const { status } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/user/login`, data)
       if (status === 200) {
         router.push('/')
@@ -49,6 +53,8 @@ function LoginPage () {
           message: 'Usuário ou e-mail não encontrado.'
         })
       }
+    } finally {
+      setLoading(false)
     }
   }
   return (
@@ -60,7 +66,7 @@ function LoginPage () {
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Input label="Email ou usuário" name="userOrEmail" control={control} /> 
           <Input label="Senha" type="password" name="password" control={control} />
-          <Button type='submit' disabled={Object.keys(errors).length > 0}>Entrar</Button>
+          <Button Loading={loading} type='submit' disabled={Object.keys(errors).length > 0}>Entrar</Button>
         </Form>
         <Text>Não possui uma conta? <Link href="/signup">Faça seu cadastro</Link></Text>
       </FormContainer>
